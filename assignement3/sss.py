d8ade15cc008e4b259db40d2941fdd768af3cdc9
Matteo294 <zmatteo.29@gmail.com>
2021-05-26 14:12:22 +0200

    assignements done

--- assignement3/sss.py
+++ assignement3/sss.py
@@ -13,7 +13,7 @@ from sympy.functions.elementary.miscellaneous import root
 
 eV = 1.6e-019; 
 kB = 1.38e-023; # Boltzmann constant, J/mol/K
-h= 6.626e-034; # Planck's constant, Js
+h = 6.626e-034; # Planck's constant, Js
 m_0 = 9.1e-031; # Rest mass of an electron, kg
 #Parameters
 
@@ -23,7 +23,7 @@ Eg = 1.1*eV; # Energy gap of Si #setting E_v=0
 Nd = 1e17
 deltaE = 0.045*eV 
 Ed=(Eg-deltaE) #Energy of the donor band
-
+Nc = lambda T: 2*(2*np.pi*kB*T/h**2)**(3/2)*(meff_elect)**(3/2)/1e6
 
 #functions
 def n(Ef,T): #carrier distribution in conduction
@@ -36,12 +36,20 @@ def g(Ef,T): # neutral charge equation
     return  Ndp(Ef,T) + p(Ef,T) - n(Ef,T)
 def highT(Ef,T):
     return ni(T)
+def NdplowT(T):
+    return np.sqrt(Nc(T)*Nd/2) * np.exp(-(Eg-Ed)/(2*kB*T))
 
 
-Efi= lambda T: Eg/2 + (3/4)*kB*T*np.log(meff_hole/meff_elect) #intrinsic fermi energy
+def Efi(T):
+    if isinstance(T, (int, float)):
+        print("yes")
+        return Eg/2 + (3/4)*kB*T*np.log(meff_hole/meff_elect)
+    else:
+        res = [Eg/2 + (3/4)*kB*TT*np.log(meff_hole/meff_elect) for TT in T]
+        return np.array(res) #intrinsic fermi energy
 ni= lambda T: 2*(2*np.pi*kB*T/h**2)**(3/2)*(meff_elect*meff_hole)**(3/4)*np.exp(-Eg/(2*kB*T)) / 10**6
 
-Tvals = np.linspace(70,1300,1000)
+Tvals = np.linspace(50,1500,1000)
 Tvals1 = [1000/T for T in Tvals]
 root1=np.ones(len(Tvals))
 c=0
@@ -50,27 +58,40 @@ for i in Tvals:
     print(root1[c])
     c +=1 
 eg= np.linspace(0,1.1,len(Tvals))
-plt.plot(Tvals1,root1/eV)
-plt.plot(Tvals1,Efi(Tvals)/eV)
-plt.plot(Tvals1,Eg*np.ones(len(Tvals))/eV,'--')
-plt.plot(Tvals1,Ed*np.ones(len(Tvals))/eV,'--')
-plt.plot(Tvals1,0*np.ones(len(Tvals)),'--')
-plt.xlabel(r"$1000/T$")
-plt.ylabel(r"$E [eV]$")
+plt.plot(Tvals1,root1/eV, label=r'$E_f$', color='mediumspringgreen', linewidth=2.0)
+plt.plot(Tvals1,Efi(Tvals)/eV, label=r'$E_i$', color='deepskyblue', linewidth=2.0)
+plt.text(0.8, Eg/eV+0.015, "Conduction band", fontsize=10)
+plt.text(0.8, Ed/eV-0.04, "Donors level", fontsize=10)
+plt.text(0.8, 0.02, "Valence band", fontsize=10)
+plt.plot(Tvals1,Eg*np.ones(len(Tvals))/eV,'--', color='gray')
+plt.plot(Tvals1,Ed*np.ones(len(Tvals))/eV,'--', color='gray')
+plt.plot(Tvals1,0*np.ones(len(Tvals)),'--', color='gray')
+plt.xlabel(r"$1000/T$", fontsize=14)
+plt.ylabel(r"$E \ [eV]$", fontsize=14)
+plt.ylim([0, 1.3])
 plt.grid()
+plt.legend(fontsize=12)
 plt.show()
-plt.semilogy(Tvals1, n(root1,Tvals))
+
+plt.semilogy(Tvals1, n(root1,Tvals), linewidth=2.2, label='n(T)', color='black')
 plt.grid()
 
-low_T = np.linspace(100, 300, 1000)
-med_T = np.linspace(300, 500, 1000)
+low_T = np.linspace(50, 300, 1000)
+med_T = np.linspace(150, 1500, 1000)
 high_T = np.linspace(500, 1500, 1000)
-low_x = [1000/T for T in low_T]
-med_x = [1000/T for T in med_T]
-high_x = [1000/T for T in high_T]
+lowT_x = [1000/T for T in low_T]
+medT_x = [1000/T for T in med_T]
+highT_x = [1000/T for T in high_T]
+print(low_T)
+print(Efi(low_T))
 
-plt.plot(low_x, Ndp(E))
-
+print(Ndp(Efi(low_T), low_T))
+plt.semilogy(highT_x, ni(high_T), '--', label='Intrinsic', color='deepskyblue', linewidth=1.8)
+plt.semilogy(medT_x, np.full(len(medT_x), Nd), '--', label='Extrinsic', color='limegreen', linewidth=1.8)
+plt.semilogy(lowT_x, NdplowT(low_T), '--', label='Freeze-out', color='red', linewidth=1.8)
+plt.legend(fontsize=12)
+plt.ylabel(r'Concetration [$cm^{-2}$]', fontsize=14)
+plt.xlabel('1000/T', fontsize=14)
 plt.show()
 #print(g(root1,Tvals))
 #plt.semilogy(eg,n(root1,Tvals)+Ndp(root1,Tvals))
